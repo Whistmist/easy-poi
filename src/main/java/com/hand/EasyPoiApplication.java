4c0614c0139c0b53f995a5be7a568fd7531535d9
mist <505168746@qq.com>
2020-02-15 22:03:44 +0800

    project init

--- src/main/java/com/hand/EasyPoiApplication.java
+++ src/main/java/com/hand/EasyPoiApplication.java
@@ -3,13 +3,22 @@ package com.hand;
 import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAsync
 public class EasyPoiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyPoiApplication.class, args);
+    }
+
+    @GetMapping("/get")
+    public String inds(){
+        return "de";
     }
 
 }
